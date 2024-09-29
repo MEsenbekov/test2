746cd17ac66b493021b78ab85db65c1eaf9ac484
MEsenbekov <maksatdaniar30@gmail.com>
2024-09-30 00:11:18 +0600

    test2

--- tasks/views.py
+++ tasks/views.py
@@ -2,10 +2,26 @@ from django.views.generic import ListView, DetailView, CreateView
 from .models import Task
 from .forms import TaskForm
 
+from django.views.generic import ListView
+from .models import Task, Category
+
 
 class TaskListView(ListView):
     model = Task
     template_name = 'tasks/task_list.html'
+    context_object_name = 'tasks'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        category_id = self.request.GET.get('category')
+        if category_id:
+            queryset = queryset.filter(category_id=category_id)
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()  # Добавьте все категории в контекст
+        return context
 
 
 class TaskDetailView(DetailView):
